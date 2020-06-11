11b081fd14631d8d3c6585e0222289c59b492731
Jahangir khan <khan03148@gmail.com>
2020-06-11 14:20:48 +0200

    project setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React mock-up for digital stage</h1>
+      </div>
+    );
+  }
+}
